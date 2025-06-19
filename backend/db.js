dc75f37af2ce8cfc7b48eeb0554ee295f492a893
Saurav Kumar <65460991+saurav79284@users.noreply.github.com>
2025-06-20 02:04:37 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -6,20 +6,19 @@ const mongoose = require('mongoose')
 const mongoURI = "mongodb+srv://hs26042004markam:26042004@cluster0.epdzw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 module.exports = function (callback) {
-    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }).then(async (result) => {
         // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
-        if (err) console.log("---" + err)
-        else {
+       
             // var database =
             console.log("connected to mongo")
             const foodCollection = await mongoose.connection.db.collection("food_items");
             foodCollection.find({}).toArray(async function (err, data) {
                 const categoryCollection = await mongoose.connection.db.collection("Categories");
-                categoryCollection.find({}).toArray(async function (err, Catdata) {
-                    callback(err, data, Catdata);
-
-                })
-            });
+                categoryCollection.find({}).toArray(async function (Catdata) {
+                    callback(data, Catdata);
+                }).catch((error)=>{
+                    console.log("database not connected");
+                }
             // listCollections({name: 'food_items'}).toArray(function (err, database) {
             // });
             //     module.exports.Collection = database;
